28fb50fecee961891542b259e91295ead1890d89
Yxy-SY <yuxiuyi@163.com>
2018-05-14 23:32:37 +0800

    commit project first

---
+++ YXYWaterflow/YXYWaterflow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YXYWaterflow
+//
+//  Created by yuxiuyi on 2018/5/14.
+//  Copyright © 2018年 yuxiuyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YXYWaterflow/YXYWaterflow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YXYWaterflow
+//
+//  Created by yuxiuyi on 2018/5/14.
+//  Copyright © 2018年 yuxiuyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
